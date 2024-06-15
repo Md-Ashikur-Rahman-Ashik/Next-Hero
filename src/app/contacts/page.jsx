a3eb941c66b85af27c9b89d1be7c909f2b9ede08
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-15 17:26:44 +0600

    Added contacts page

---
+++ src/app/contacts/page.jsx
@@ -0,0 +1,12 @@
+import AboutContents from "@/components/AboutContents";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="card card-compact container rounded-xl mx-auto max-w-fit">
+      <AboutContents></AboutContents>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/AboutContents.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutContents = () => {
+  return (
+    <div>
+      <h2 className="font-bold text-6xl">About Contents</h2>
+    </div>
+  );
+};
+
+export default AboutContents;
