36092091d509691b788659a1c0c532f0aa4e0070
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-15 19:42:38 +0600

    Added vision page

--- src/components/AboutContents.jsx
+++ src/components/AboutContents.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AboutContents = () => {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 font-bold">
       <Link href={"/about/history"}>History</Link>
       <Link href={"/about/mission"}>Mission</Link>
     </div>
