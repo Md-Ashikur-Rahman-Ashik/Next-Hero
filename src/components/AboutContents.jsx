c0681c7ead98a6e3871c0ba8870a4f152bfb62fd
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-15 19:35:03 +0600

    Added space between two links

--- src/components/AboutContents.jsx
+++ src/components/AboutContents.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AboutContents = () => {
   return (
-    <div>
+    <div className="flex gap-2">
       <Link href={"/about/history"}>History</Link>
       <Link href={"/about/mission"}>Mission</Link>
     </div>
