3932be4952efe860f4c409f5f01dfcc9f8ef4308
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-15 21:57:11 +0600

    Added categories details page

---
+++ src/app/categories/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="card card-compact container rounded-xl mx-auto max-w-fit">
+      <h2 className="font-bold text-6xl">Categories Page</h2>
+    </div>
+  );
+};
+
+export default page;
