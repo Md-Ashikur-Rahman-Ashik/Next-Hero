f526e91d8da23c7e900e937ccd5474d2a8c4815b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-20 23:04:12 +0600

    Implemented dynamic data fetching

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-const page = ({ params }) => {
+const getDetails = async (id) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+  return data;
+};
+
+const page = async ({ params }) => {
+  const { title, body } = await getDetails(params.id);
+
   return (
     <div className="card card-compact container rounded-xl mx-auto">
-      <h2 className="text-6xl font-bold flex justify-center">
-        Post Details Of {params.id}
+      <h2 className="text-6xl font-bold text-center mb-6 flex justify-center">
+        Title: {title}
+      </h2>
+      <h2 className="text-3xl text-center flex justify-center">
+        Description: {body}
       </h2>
     </div>
   );
