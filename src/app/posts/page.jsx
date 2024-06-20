349191ae5ec954e616dd8f6acf8aa07c1483f844
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-20 22:39:44 +0600

    Implemented advanced data fetching with SSR

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,20 @@
+import { postsData } from "@/services/postAPI";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const postData = await postsData();
+
   return (
-    <div className="card card-compact container rounded-xl mx-auto max-w-fit">
-      <h2 className="font-bold text-6xl">Posts Page</h2>
+    <div className="card card-compact container rounded-xl mx-auto">
+      <h2 className="font-bold text-6xl flex justify-center mb-4">All Posts</h2>
+      <div className="grid grid-cols-4 gap-6">
+        {postData?.slice(0, 20)?.map((post) => (
+          <div key={post.id} className="border-2 p-6">
+            <h6 className="text-center font-bold mb-4">{post.title}</h6>
+            <h6>{post.body}</h6>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
