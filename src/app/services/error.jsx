4eaef97f6c94b8b8741b8f3db7ba28302374485f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-20 18:02:43 +0600

    Added error error on service page

---
+++ src/app/services/error.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const error = () => {
+  return (
+    <div className="card card-compact container rounded-xl mx-auto max-w-fit">
+      <h2 className="font-bold text-6xl capitalize">Service Error</h2>
+    </div>
+  );
+};
+
+export default error;
