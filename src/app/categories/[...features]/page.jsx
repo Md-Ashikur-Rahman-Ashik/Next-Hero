74efe950a26dec1df8bf6cd98d74169561b80c86
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-20 10:00:39 +0600

    Added dynamic routes page

--- src/app/categories/[...features]/page.jsx
+++ src/app/categories/[...features]/page.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
-const page = () => {
+const page = ({ params }) => {
+  if (params.features.length === 3) {
+    return (
+      <div className="card card-compact container rounded-xl mx-auto max-w-fit">
+        <h2 className="font-bold text-6xl capitalize">{params.features[2]}</h2>
+      </div>
+    );
+  } else if (params.features.length === 2) {
+    return (
+      <div className="card card-compact container rounded-xl mx-auto max-w-fit">
+        <h2 className="font-bold text-6xl capitalize">{params.features[1]}</h2>
+      </div>
+    );
+  }
+
   return (
     <div className="card card-compact container rounded-xl mx-auto max-w-fit">
       <h2 className="font-bold text-6xl">Categories Details Page</h2>
