5cce3aaa8e1f127973b57e95a72c38e06e9850a3
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-20 19:19:57 +0600

    Explored middleware

--- src/components/AboutContents.jsx
+++ src/components/AboutContents.jsx
@@ -3,9 +3,19 @@ import React from "react";
 
 const AboutContents = () => {
   return (
-    <div className="flex gap-2 font-bold">
-      <Link href={"/about/history"}>History</Link>
-      <Link href={"/about/mission"}>Mission</Link>
+    <div className="flex gap-4 text-6xl font-bold">
+      <Link
+        className="border-8 border-blue-100 p-2 rounded-xl"
+        href={"/about/history"}
+      >
+        History
+      </Link>
+      <Link
+        className="border-8 border-blue-100 p-2 rounded-xl"
+        href={"/about/mission"}
+      >
+        Mission
+      </Link>
     </div>
   );
 };
--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,24 @@
 import { NextResponse } from "next/server";
 
+// const user = true;
+const coo = "NextSuperHero";
+
 export const middleware = (request) => {
-  return NextResponse.redirect(new URL("/about", request.url));
+  //   if (!user) {
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   } else {
+  //     return NextResponse.next();
+  //   }
+
+  const cookies = request.cookies.get("token");
+
+  if (!cookies || cookies.value !== coo) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  } else {
+    return NextResponse.next();
+  }
 };
 
 export const config = {
-  matcher: "/user-profile",
+  matcher: ["/dashboard", "/services"],
 };
