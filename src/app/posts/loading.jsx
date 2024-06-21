2055acb66b80720b9fb3aed3e242daf8f0dcce20
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-21 22:15:31 +0600

    Implemented loader

---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="container rounded-xl mx-auto">
+      <h2 className="font-bold text-center text-6xl capitalize">
+        Post Data Is Loading.....
+      </h2>
+    </div>
+  );
+};
+
+export default loading;
