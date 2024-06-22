482a49267303f2078fd914271b49dcfd4a653c29
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-22 16:44:49 +0600

    Handling api routes

---
+++ src/app/comments/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container rounded-xl mx-auto">
+      <h2 className="font-bold text-6xl flex justify-center">Comment Page</h2>
+    </div>
+  );
+};
+
+export default page;
