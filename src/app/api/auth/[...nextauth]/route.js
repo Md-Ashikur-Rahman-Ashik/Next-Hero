f6907b02a9446cd05484e0f45db2840f885d4491
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-06-23 10:03:02 +0600

    Got NextAuth session data in client  components
    Also, got session data in server components

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,52 @@
 import NextAuth from "next-auth/next";
-
+import CredentialsProvider from "next-auth/providers/credentials";
 const handler = NextAuth({
   session: {
     strategy: "jwt",
   },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          required: true,
+          placeholder: "Your Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          required: true,
+          placeholder: "Enter Password",
+        },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        if (!credentials) {
+          return null;
+        } else if (email) {
+          const currentUser = users.find((user) => user.email === email);
+          if (currentUser) {
+            if (currentUser.password === password) {
+              return currentUser;
+            }
+          }
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 });
 
 export { handler as GET, handler as POST };
+
+const users = [
+  {
+    id: 1,
+    name: "Ashik",
+    email: "ashik@gmail.com",
+    password: "password",
+  },
+];
